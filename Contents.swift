10056657b95c194cb23784266d8ab888845b06a1
Apple <apple@Apples-MacBook-Pro-3.local>
2019-07-22 15:02:27 -0400

    initialized git

---
+++ Contents.swift
@@ -0,0 +1,47 @@
+/* string - text you want the computer to say exactly back to you
+ integer - whole numbers including 0 & negative #s
+ double - stores numbers with decimals; more accuracy than floats
+ interpolation -
+ variable -
+ constant -
+ keyword-
+ 
+ */
+
+import Cocoa
+
+var str = "Hello, playground"
+var address = "1313 Mockingbird Lane"
+address = "254 Ocean Avenue"
+
+let name = "Karlie"
+var empty:Double
+
+
+
+
+var printMe = "hello world"
+print (printMe)
+
+var name1 = "Ronke"
+var lastName = "Adejare"
+print(name1 + " " + lastName)
+print(name1,lastName)
+print("Hi my name is \(name1). My favorite number is \(3*3)." )
+
+print("What is 10 * 10 - 3? The answer is \(10*10-3)" )
+// 9.7 & -3
+print("What is the quotient of 9.7 and the divsor -3? The quotient is -\(9.7/3)" )
+
+var temp:Double
+temp = 79
+if temp > 80 {
+    print("wear shorts") }
+else {
+print("wear pants" )}
+
+if temp == Double(87) {
+    print("I guessed correct")
+}
+else { print("WRONG! not 87")}
+
